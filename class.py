223d4d4121f67bdb42ae685b10210802ab791c5d
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-03 11:29:02 +0530

    Update class.py

--- class.py
+++ class.py
@@ -26,3 +26,94 @@ class Triangle:
 
 e = Triangle(3,5,6)
 print(e.perimeter())
+
+-------------------------inheritance-------------------------
+class Polygon:
+    def __init__(self, sides):
+        self.sides = sides
+
+    def display_info(self):
+        print("A polygon is a two dimensional shape with straight lines")
+
+    def get_perimeter(self):
+        perimeter = sum(self.sides)
+        return perimeter
+
+
+class Triangle(Polygon):
+    def display_info(self):
+        print("A triangle is a polygon with 3 edges")
+
+
+class Quadrilateral(Polygon):
+    def display_info(self):
+        print("A quadrilateral is a polygon with 4 edges")
+        
+        
+t1 = Triangle([5, 6, 7])
+perimeter = t1.get_perimeter()
+print("Perimeter:", perimeter)        
+
+t1.display_info()   #Method overwriting
+
+############method overwirting###########
+class Polygon:
+    def __init__(self, sides):
+        self.sides = sides
+
+    def display_info(self):
+        print("A polygon is a two dimensional shape with straight lines")
+
+    def get_perimeter(self):
+        perimeter = sum(self.sides)
+        return perimeter
+
+
+class Triangle(Polygon):
+    def display_info(self):
+        print("A triangle is a polygon with 3 edges")
+        Polygon.display_info(self)
+
+
+class Quadrilateral(Polygon):
+    def display_info(self):
+        print("A quadrilateral is a polygon with 4 edges")
+
+t1 = Triangle([5, 6, 7])
+perimeter = t1.get_perimeter()
+print("Perimeter:", perimeter)
+
+t1.display_info()
+        
+#################super() function###############3
+here super() is the object of Polygon, super is not a class
+
+class Polygon:
+    def __init__(self, sides):
+        self.sides = sides
+
+    def display_info(self):
+        print("A polygon is a two dimensional shape with straight lines")
+
+    def get_perimeter(self):
+        value = 0
+        for side in self.sides:
+            value += side
+        return value
+
+
+class Triangle(Polygon):
+    def display_info(self):
+        print("A triangle is a polygon with 3 edges")
+        # Polygon.display_info(self)
+        super().display_info()
+
+class Quadrilateral(Polygon):
+    def display_info(self):
+        print("A quadrilateral is a polygon with 4 edges")
+
+t1 = Triangle([5, 6, 7])
+perimeter = t1.get_perimeter()
+print("Perimeter:", perimeter)
+
+t1.display_info()
