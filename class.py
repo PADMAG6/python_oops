aefa17ba14c7016c81ad7fd60731b783a6d196f6
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-03 10:59:19 +0530

    Update class.py

--- class.py
+++ class.py
@@ -10,3 +10,19 @@ a.name = "padma"
 a.marks = 20
 did_pass = a.check_pass_fail()
 print(did_pass)
+
+
+
+__________________triangle_perimeter________________--
+class Triangle:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def perimeter(self):
+        P = self.a + self.b + self.c
+        return P
+
+e = Triangle(3,5,6)
+print(e.perimeter())
